3766f9a341a19e120f6c38eb53fed9f2f7294232
ye chan <jkyyc3@gmail.com>
2024-02-15 22:42:21 +0900

    feat: Tooltip

--- src/components/tooltip/Tooltip.jsx
+++ src/components/tooltip/Tooltip.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import classNames from "classnames";
+import styles from "./Tooltip.module.scss";
 
-const Tooltip = () => {
-  return <div>TOOLTIP</div>;
+const Tooltip = ({
+  top = 0,
+  right = 0,
+  bottom = 0,
+  left = 0,
+  color = "",
+  bgColor = "",
+  orientation = "top",
+  message,
+  ...restProps
+}) => {
+  const style = {
+    top,
+    right,
+    bottom,
+    left,
+    color,
+    backgroundColor: bgColor,
+  };
+
+  const setOrientationClass = (type) => {
+    switch (type) {
+      case "top":
+        return styles.orientationTop;
+      case "right":
+        return styles.orientationRight;
+      case "left":
+        return styles.orientationLeft;
+      case "bottom":
+        return styles.orientationBottom;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <span
+      role={"tooltip"}
+      style={style}
+      className={classNames(styles.tooltip, setOrientationClass(orientation))}
+    >
+      {message}
+    </span>
+  );
 };
 
 export default Tooltip;
