ba31adf21ecad88bc84c1173ae31cd842da061bc
ye chan <jkyyc3@gmail.com>
2024-02-19 23:36:48 +0900

    feat: 비밀번호 리셋

--- src/app/(auth)/reset/page.jsx
+++ src/app/(auth)/reset/page.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import ResetClient from "@/app/(auth)/reset/ResetClient";
 
 const Reset = () => {
-	return (
-		<div>
-			
-		</div>
-	);
+  return (
+    <div>
+      <ResetClient />
+    </div>
+  );
 };
 
 export default Reset;
--- src/components/heading/Heading.jsx
+++ src/components/heading/Heading.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Heading = () => {
-  return <div></div>;
+  return <div>Heading</div>;
 };
 
 export default Heading;
